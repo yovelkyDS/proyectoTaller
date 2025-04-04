d058abed18cd6cf51ec53babb605146dd7c54137
yovelky <yovelkydelso260106@gmail.com>
2025-04-03 22:24:11 -0600

    Creacion de archivos completa

--- proyectoSinInterfaz/cli.py
+++ proyectoSinInterfaz/cli.py
@@ -1,18 +1,24 @@
 import shutil
-from files import openConversation, register
+from files import openConversation, chat
+
+def centerText(txt:str)->str:
+    terminal = shutil.get_terminal_size()
+    textoC = txt.center(terminal.columns)
+    return textoC
 
 def login()->str:
-    terminal = shutil.get_terminal_size()
-    print("ChatBot".center(terminal.columns))
+    print(centerText("ChatBot"))
     nombre = str(input("Digite su nombre de usuario: "))
     return nombre
 
-def chatMsg()->str:
-    
-
-
+def chatMsg(n:str):
+    print(centerText("ChatBot"))
+    question = str(input("¡Hola! En que puedo ayudarte?\nTu: "))
+    answer = chat(question, n)
+    print(f"Respuesta: {answer}")
 
 if __name__ == "__main__":
     name = login()
+    history = openConversation(name)
     while True:
-        pass
+        chatMsg(name)
--- proyectoSinInterfaz/files.py
+++ proyectoSinInterfaz/files.py
@@ -1,8 +1,7 @@
 from API import chat_con_php 
-from cli import login
 
-def openConversation()->bool:
-    nameF = f"{login()}_conversation.txt"
+def openConversation(name:str)->bool:
+    nameF = f"{name}_conversation.txt"
     with open(nameF, "a+") as file:
         file.seek(0)
         content = file.readlines()
@@ -24,3 +23,18 @@ def register(msg:str, name:str,) -> str:
         fileN = f"{name}_conversation.txt"
         with open(fileN, "a", encoding="utf-8") as f:
             f.write(f"{msg}\n")
+
+def chat(message:str, nombre:str)-> str:
+    """Trae la informacion que proporciona el usuario, registra y devuelve la respuesta del chatbot
+
+    Args:
+        message (str): mensaje del usuario
+
+    Returns:
+        str: respuesta del chatbot
+    """
+    answer = chat_con_php(message)
+    conversacion = []
+    conversacion.append(["Usuario:", message, "Chatbot", answer])
+    register(conversacion, nombre)
+    return answer
